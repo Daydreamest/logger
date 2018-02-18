dac4fdce4c6e14c9b4254842a6f3877e6c7e06be
Miau <tankorn@wp.pl>
2018-02-18 15:17:53 +0100

    Second logger with log level

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <Logger1.h>
+#include <Logger2.h>
 
 int main()
 {
-
     // Simple cout logging
     Logger1 logger1;
     logger1.log("Logger 1 test");
+
+    // Simple cout logging with log levels
+    Logger2 logger2;
+    logger2.log("Logger2 test A");
+    logger2.setLevel(LogLevel_Info);
+    logger2.log("Logger2 test B");
 
     return 0;
 }
